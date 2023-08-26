0ae166372b2f66ca388f9c6e1557e312ed2b19e0
Riteshkumar Chaudhari <riteshc7299@gmail.com>
2023-08-25 19:29:08 -0700

    Added Welcome

---
+++ src/main/java/com/example/demojavacicd/Welcome.java
@@ -0,0 +1,14 @@
+package com.example.demojavacicd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+
+    @GetMapping("/welcome")
+    public String grtNotes() {
+        return "Welcome to the project";
+    }
+
+}
